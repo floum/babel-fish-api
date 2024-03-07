3bbe603f2acc1eb7de4e1088cd9ea70d8014e06f
Efflam Castel <efflamm.castel@gmail.com>
2024-03-07 19:32:11 +0000

    initial commit

---
+++ src/main/java/org/babelfish/babelfishapi/BabelFishApiApplication.java
@@ -0,0 +1,13 @@
+package org.babelfish.babelfishapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BabelFishApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BabelFishApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/babelfish/babelfishapi/BabelFishApiApplicationTests.java
@@ -0,0 +1,13 @@
+package org.babelfish.babelfishapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BabelFishApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
